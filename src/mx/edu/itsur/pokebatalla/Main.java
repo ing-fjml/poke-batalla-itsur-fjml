328865c0c9c1aa138ca8021f5219e083ae23e80e
OsvielA <jorgeosviel@gmail.com>
2023-10-05 21:24:18 -0600

    se subio el codigo con los tres pokemones

--- src/mx/edu/itsur/pokebatalla/Main.java
+++ src/mx/edu/itsur/pokebatalla/Main.java
@@ -2,11 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 package mx.edu.itsur.pokebatalla;
 
 import mx.edu.itsur.pokebatalla.model.Bullbasaur;
 import mx.edu.itsur.pokebatalla.model.Pikachu;
 import mx.edu.itsur.pokebatalla.model.Charmander;
+import mx.edu.itsur.pokebatalla.model.Psyduck;
+import  mx.edu.itsur.pokebatalla.model.Snorlax;
+import  mx.edu.itsur.pokebatalla.model.Gengar;
+
 
 /**
  *
@@ -23,15 +28,42 @@ public class Main {
         
         Pikachu pikachuSalvaje = new Pikachu();
         Charmander charmanderSalvaje = new Charmander() ;
+        //declarando los pokemones mios 
+        Psyduck psyduckSalvaje= new Psyduck();
+        Snorlax snorlaxSalvaje= new Snorlax();
+        Gengar gengarSalvaje= new Gengar();
         
+
         //Realizar la captura del pikachu salvaje
         pikachuSalvaje.setNombre("Pedro");        
         Pikachu miPikachu = pikachuSalvaje;
         
         miPikachu.atacar(charmanderSalvaje);
-        
         miPikachu.atacar(charmanderSalvaje, "ATACKTRUENO");
+       
+        ///captura de psyduck salvaje 
+         psyduckSalvaje.setNombre("panchito");
+         Psyduck miPsyduck = psyduckSalvaje;
+     ///captura de Snorlax salvaje 
+     snorlaxSalvaje.setNombre("avionado");
+         Snorlax miSnorlax = snorlaxSalvaje;
+             ///captura de Gengar salvaje 
+           gengarSalvaje.setNombre("bolita");
+         Gengar migengar = gengarSalvaje;
+         
+         //realizando batallas 
+          miPsyduck.atacar(charmanderSalvaje);
+        miPsyduck.atacar(charmanderSalvaje, "PISTOLAAGUA");
         
-    }
+          miSnorlax.atacar(pikachuSalvaje);
+        miSnorlax.atacar(pikachuSalvaje, "TERRATEMBLOR");
+       
+            migengar.atacar(snorlaxSalvaje);
+        migengar.atacar(snorlaxSalvaje, "CARASUSTO");
+       
+    }       
+
     
+
+   
 }
