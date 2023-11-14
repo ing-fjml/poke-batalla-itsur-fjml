137dca2626f3dfa89786a5df4cef0934745844ca
FatimaGM <136218946+FatimaGM@users.noreply.github.com>
2023-11-13 22:31:11 -0600

    Add files via upload

--- src/mx/edu/itsur/pokebatalla/Main.java
+++ src/mx/edu/itsur/pokebatalla/Main.java
@@ -4,6 +4,8 @@
  */
 package mx.edu.itsur.pokebatalla;
 
+import mx.edu.itsur.pokebatalla.model.battles.Batalla;
+import mx.edu.itsur.pokebatalla.model.battles.Entrenador;
 import mx.edu.itsur.pokebatalla.model.pokemons.Bullbasaur;
 import mx.edu.itsur.pokebatalla.model.pokemons.Pikachu;
 import mx.edu.itsur.pokebatalla.model.pokemons.Charmander;
@@ -16,59 +18,30 @@ import mx.edu.itsur.pokebatalla.model.pokemons.Squirtle;
  * @author Fatima Gutierrez Moreno
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
         System.out.println("Hello PokeBatalla!");
-        
-        Pikachu pika = new Pikachu();
-        Charmander charm = new Charmander();
-        Bullbasaur bullb = new Bullbasaur();
-                
-        System.out.println("->Pikachu se encuentra con un Charmander y ataca");
-        pika.atacar(charm, Pikachu.Movimientos.IMPACTRUENO.ordinal());
-        
-        System.out.println("->Charmander se molesta y responde ");
-        charm.atacar(pika, Charmander.Movimientos.ATAQUE_RAPIDO.ordinal());
-     
-        System.out.println("->Bullbasaur se enoja porque atacaron a su amigo Pikachu y...");
-        bullb.atacar(charm, Bullbasaur.Movimientos.LATIGO.ordinal());
-        
-        //Pelea creada por la organizadora Fatima :)
-        //Despues de la anterior pelea se decidio realizar otro encuentro entre estos
-        //nuevos pokemones...La pelea comenzara en 1,2,3...Y estos son los pokemones 
-        //que estaran en el  ring esta noche, disfrute de la pelea y apueste por su favorito...:)
-        Squirtle blue=new Squirtle();
-        Dragonite turbo=new Dragonite();
-        Oddish cookie=new Oddish();
-        
-         System.out.println("->Squirtle se encuentra en el ring con Dragonite y ataca");
-        blue.atacar(turbo, Squirtle.Movimientos.Cascada.ordinal());
-        
-         System.out.println("->Dragonite se molesto por ese tremendo golpe y responde ");
-        turbo.atacar(blue, Dragonite.Movimientos.Furia_Dragon.ordinal());
-        
-        System.out.println("->OOOH...Oddish se molesto porque ese movimiento no le parecio justo "
-                + "y interviene en la pelea , esto se puso muy interesante...");
-        cookie.atacar(turbo, Oddish.Movimientos.Danza_Petalo.ordinal());
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+         Oddish suga= new Oddish("suga");
+        Dragonite guason = new Dragonite("guason");
+        Squirtle azul = new Squirtle(" azul");
+         Charmander pink = new Charmander ("pink");
+         Pikachu Pika = new Pikachu ("pika");
+          Bullbasaur Bull= new Bullbasaur("BUll");
+          
+          Entrenador entrenador1 = new Entrenador("J-Hope");
+       entrenador1.capturarPokemon(suga);
+       entrenador1.capturarPokemon(guason);
+       
+       Entrenador entrenador2 = new Entrenador("Jungkook");
+       entrenador2.capturarPokemon(azul);
+       entrenador2.capturarPokemon(pink);
+       
+       Batalla batalla = new Batalla(entrenador1, entrenador2);
+       batalla.desarrollarBatalla();
         
     }
-    
 }
+
+    
